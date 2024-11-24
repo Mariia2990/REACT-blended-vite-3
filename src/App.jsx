d917008a409a5084425078d17fc02509398e1408
Elena Zhuvak <elenkazh@gmail.com>
2024-11-24 10:25:05 +0000

    adds home-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Country } from 'pages/Country';
 import { Home } from 'pages/Home';
 import { SearchCountry } from 'pages/SearchCountry';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Header } from './components';
 
 export const App = () => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/country/" element={<SearchCountry />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,36 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+
+  useEffect(()=>{
+    const getData = async () => {
+      try {
+        setIsLoading(true)
+        setErrorMsg('')
+        const data = await getCountries()
+        setCountries(data)
+        
+      } catch (error) {
+        setErrorMsg(error.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    getData()
+    
+    
+  },[])
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+       <CountryList countriesData={countries}/>
+       {isLoading && <Loader/>}
+       {errorMsg && <p>{errorMsg}</p>}
       </Container>
     </Section>
   );
